9a4dc3d45d153dd473c123306efd7ca451db4b09
Diego Martínez <diegomrtnzg@users.noreply.github.com>
2017-11-16 11:35:31 +0100

    UpdateTemplate

--- Check Point/CheckPointExtra/CheckPointExtra/CheckPointExtra/Deploy-AzureResourceGroup.ps1
+++ Check Point/CheckPointExtra/CheckPointExtra/CheckPointExtra/Deploy-AzureResourceGroup.ps1
@@ -2,7 +2,7 @@
 
 Param(
     [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
-    [string] $ResourceGroupName = 'CheckPointARM',
+    [string] $ResourceGroupName = 'CheckPointExtra',
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
     [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
