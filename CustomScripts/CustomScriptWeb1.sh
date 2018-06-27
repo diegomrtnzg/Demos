ad5e5cafb0760f7ef91301c4df0eceef1955ae76
Diego Martínez <diegomrtnzg@users.noreply.github.com>
2018-06-27 19:14:07 +0200

    Custom scripts

---
+++ CustomScripts/CustomScriptWeb1.sh
@@ -0,0 +1,10 @@
+# Custom Script for Linux
+#!/bin/bash
+
+# update package source
+apt-get -y update
+
+# install NGINX
+apt-get -y install nginx
+
+echo "Hello World from host" $HOSTNAME " from Web Server 1!" | sudo tee -a /var/www/html/index.html
